4198c0dcda6285db61c85a8f1e4deffaffb3defa
Servet Arslan <arslannservett@gmail.com>
2022-08-10 13:27:36 +0300

    Security is temporarily excluded

--- src/main/java/com/servetarslan/todolistapp/TodolistappApplication.java
+++ src/main/java/com/servetarslan/todolistapp/TodolistappApplication.java
@@ -2,8 +2,12 @@ package com.servetarslan.todolistapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+//Security inactive
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
+//@SpringBootApplication
 public class TodolistappApplication {
 
 	public static void main(String[] args) {
