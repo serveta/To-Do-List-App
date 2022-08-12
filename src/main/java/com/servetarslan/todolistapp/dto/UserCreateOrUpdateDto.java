8967eb4546e9d6ce07db050d1cc9daf50c149c37
Servet Arslan <arslannservett@gmail.com>
2022-08-12 12:59:33 +0300

    Modified UserDto, Created UserCreateOrUpdateDto

--- src/main/java/com/servetarslan/todolistapp/dto/UserCreateOrUpdateDto.java
+++ src/main/java/com/servetarslan/todolistapp/dto/UserCreateOrUpdateDto.java
@@ -1,28 +1,20 @@
 package com.servetarslan.todolistapp.dto;
 
-import com.servetarslan.todolistapp.model.Role;
-import com.servetarslan.todolistapp.model.ToDoList;
-import com.servetarslan.todolistapp.repository.RoleRepository;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.hibernate.tool.schema.internal.exec.GenerationTarget;
-
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Log4j2
-public class UserDto {
-    private Long id;
+public class UserCreateOrUpdateDto {
     private String firstName;
     private String lastName;
     private String username;
     private String mail;
     private String password;
-    private RoleDto role;
 }
